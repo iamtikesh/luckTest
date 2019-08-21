dfc7725f0b51d1d2789b24358aba9d372dbb7211
Tikeshwar Deep <54026353+iamtikesh@users.noreply.github.com>
2019-08-21 15:47:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// var userName=document.getElementsById("name");
+// Tikeshwar Deep
 
 var first = Math.random();
 var second = Math.random();
@@ -11,20 +11,29 @@ second = Math.floor(second) + 1;
 
 function test() {
    var userName = document.getElementById('a').value;
+   var userName2 = document.getElementById('b').value;
+  if(a===b){second=first;}
   if (first > second) {
-    document.querySelector("h1").innerHTML = "" + userName + " ! You are Lucky ";
+    document.querySelector("user1").innerHTML =""+ userName +" , You are Lucky than your friend  "+userName2+" ";
   }
 
-  if (second >= first) {
-    document.querySelector("h1").innerHTML = "" + userName + " ! You are UnLucky ";
+  if (second > first) {
+    document.getElementById("user1").innerHTML=""+userName+" , Your friend " + userName2 + " is more lucky than you. ";
   }
 
-  document.querySelector("input").classList.add("visible");
+  if (first===second) {
+    document.getElementById("user1").innerHTML=""+userName+" , You and Your friend " + userName2 + ", both are Lucky. ";
+  }
+
+  document.querySelector("h1").innerHTML = "Refresh to Check your Luck Again.";
+  document.getElementsByTagName("input")[0].classList.add("visible");
+  document.getElementsByTagName("input")[1].classList.add("visible");
   document.querySelector("button").classList.add("visible");
-  document.getElementById("play").innerHTML = " "+ userName +" !";
+  document.getElementById("play").innerHTML = " "+ userName +" ";
+  document.getElementById("play2").innerHTML = " "+ userName2 +" ";
   document.getElementById("player").classList.remove("visible");
   document.getElementById("player2").classList.remove("visible");
   document.getElementsByTagName("img")[1].setAttribute("src", "img/dice" + second + ".png");
   document.getElementsByTagName("img")[0].setAttribute("src", "img/dice" + first + ".png");
-  document.getElementById("refresh-me").classList.remove("visible");
+
 }
